e6be5280d3674f34df2bcda0f051688269712ccf
kristopher recinos <kristopher.recinos@gmail.com>
2025-05-06 15:55:21 -0400

    Add SpeedInsights component back to layout for performance monitoring within ClientRootProvider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         <Toaster />
         <ClientRootProvider>
           {children}
+          <SpeedInsights />
         </ClientRootProvider>
       </body>
     </html>
